389af3dca726a5da76feebb4d4c27ca2a0fc5307
igornast <grezle00@gmail.com>
2023-04-22 05:29:30 +0300

    Добавлена функция. Добавлено описание к функциям

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def accepts_string(valua):
+    '''Выводит строку в верхнем регистре'''
     return valua.upper()
+
+def first_capital(valua):
+    '''Выводит строку с первыми буквами слов в верхнем регистре'''
+    return valua.title()
